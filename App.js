3b04d710b72a293fca9527a9ad6d8e7bc129d797
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-05-25 19:07:17 -0700

    Fix

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World 5/25/2022!</Text>
       <StatusBar style="auto" />
     </View>
   );
