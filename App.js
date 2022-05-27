fbd2acaf90384788768fa781039d700a420040ad
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-05-27 07:24:11 -0700

    fix

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World 5/25/2022!</Text>
+      <Text>Hello World 05/27/2022!</Text>
       <StatusBar style="auto" />
     </View>
   );
